a5d1b1305c40a2dadcf5a8c0c8400bcb5a686be8
gait2ai <abuamr.dubai.bcfhd@gmail.com>
2025-05-05 11:55:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -374,9 +374,9 @@ async def read_index():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=port,
+        port=10000,
     )
